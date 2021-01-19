ac62042c1cf14fd1d8790069b391d280768e4b5f
MirandaLem <75110265+MirandaLem@users.noreply.github.com>
2021-01-19 00:24:13 -0500

    Create counter-variable-practice.py

---
+++ counter-variable-practice.py
@@ -0,0 +1,81 @@
+#Looping Exercises
+
+#Q1: From 415 to 570 by 5
+
+count = 415
+
+while count <=570:
+    print(count)
+    count += 5 
+    
+#Q2: From 225 to 421 by 4 
+
+count = 225
+
+while count <=421:
+    print(count)
+    count += 4
+    
+ #Q3: From 88 to 176 by 8 
+
+count = 88
+
+while count <=176:
+    print(count)
+    count += 8 
+ 
+#Q4: From 13 to 761 by 17 
+
+count = 13
+
+while count <=761:
+    print (count)
+    count += 17
+
+#Q5: from -309 to -185 by 4
+
+count = -309
+
+while count <= -185:
+    print(count)
+    count += 4 
+
+#Q6: from 495 to 527 by 2 
+
+count = 495
+
+while count <=527:
+    print(count)
+    count += 2 
+
+#Q7: from -182 to -150 by 2
+
+count = -182
+
+while count <= -150:
+    print(count)
+    count += 2 
+
+#Q8: from -338 to -153 by 9
+
+count = -338 
+
+while count <= -153:
+    print(count)
+    count += 9
+
+#Q9: from -453 to -409 by 1
+
+count = -453
+
+while count <= -409:
+    print(count)
+    count += 1 
+
+#Q10: from 201 to 321 by 4
+
+count = 201
+
+while count <= 321:
+    print(count)
+    count += 4 
